9a77e372046ec18f135fe2b8b495094e40b15df3
Stewart Newnham <git@newnham.com>
2021-12-05 19:43:59 +0800

    Demo to show how margin auto works with flex

---
+++ flex_and_margin_auto/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='box-container'>
+        <div className="box">
+            Box
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
