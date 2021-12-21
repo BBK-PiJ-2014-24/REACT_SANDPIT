03b56329c9bd1a7d35541fe4e64591cf3cc5070e
Stewart Newnham <git@newnham.com>
2021-12-21 22:28:36 +0800

    Implementing useLocation() hook for sql param query strings, e.g. ?name=stewart

--- sidebar2/src/pages/Reports.jsx
+++ sidebar2/src/pages/Reports.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {useLocation} from 'react-router-dom'
 
 function Reports() {
+
+    const queryString = useLocation().search // =?name=Stewart
+    const queryParams = new URLSearchParams(queryString) // parses into key and value
+    const name = queryParams.get('name') // get value from key
+
+
     return (
         <div className='reports'>
             <h1>Reports</h1> 
+            <h2>The Report for {name}</h2>
         </div>
     )
 }
