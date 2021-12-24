311e56baa22828538907cee1b33cfadc9b1f3a6f
Stewart Newnham <git@newnham.com>
2021-12-24 23:37:52 +0800

    Added context provider

--- reducer_context/src/App.js
+++ reducer_context/src/App.js
@@ -1,24 +1,29 @@
 import {Routes, Route} from 'react-router-dom'
-import NavbarData from './assets/NavbarData'
+import HeaderProvider from './contexts/headerColor/HeaderProvider'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
 import Team from './pages/Team'
 import './App.css';
+import HeaderColorSelector from './components/HeaderColorSelector'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <h1> Hello</h1>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/team' element={<Team />} />
-      </Routes>
-    </div>
+    <HeaderProvider>
+
+      <div className="App">
+        <Navbar />
+        <HeaderColorSelector />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/team' element={<Team />} />
+        </Routes>
+      </div>
+    
+    </HeaderProvider>
   );
 }
 
