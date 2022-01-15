f06f6a6c02e8a70e738bd7535ca648ae8cfb2d20
Stewart Newnham <git@newnham.com>
2022-01-15 21:30:04 +0800

    refactored for navbar to accept prop

--- page-scrolling/src/components/Navbar.jsx
+++ page-scrolling/src/components/Navbar.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import './Navbar.css'
 
-function Navbar() {
+function Navbar({showNav}) {
+
+
+
     return (
-        <div className='navbar-container'>
-            <div className='logo'>
-                <h2>Logo</h2>
+        <>
+        {showNav && (
+            <div className='navbar-container'>
+                <div className='logo'>
+                    <h2>Logo</h2>
+                </div>
+                <ul>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Service</li>
+                    <li>Team</li>
+                </ul>  
             </div>
-            <ul>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Service</li>
-                <li>Team</li>
-            </ul>
-            
-        </div>
-    )
+        )}
+        </>
+        )
 }
 
 export default Navbar
