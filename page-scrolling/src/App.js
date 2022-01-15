4635a5e5bb67419d05ee3b18d6c88d39966bbef1
Stewart Newnham <git@newnham.com>
2022-01-15 21:27:24 +0800

    refactored toggleNav() to take an explicit boolean as an argument

--- page-scrolling/src/App.js
+++ page-scrolling/src/App.js
@@ -9,19 +9,20 @@ function App() {
 
   const [showNav, setShowNav] = useState(true)
 
-  const toggleNav = () => {
-    setShowNav(prevState => !prevState)
+  const toggleNav = (boolean) => {
+    console.log('bool', boolean)
+    setShowNav(boolean)
   }
 
   return (
     <div className="App" id='app'>
     { showNav &&
-      <Navbar showNav={showNav} toggleNav={toggleNav} />
+      <Navbar showNav={showNav} />
     } 
-      <Page id='about' name='about' color='#66545e' upPage='about' downPage='contact' />
-      <Page id='contact' name='contact' color='#aa6f73' upPage='about' downPage='service' />
-      <Page id='service' name='service' color='#eea990' upPage='contact' downPage='team' />
-      <Page id='team' name='team' color='#f6e0b5' upPage='service' downPage='team' />
+      <Page id='about' name='about' color='#66545e' upPage='about' downPage='contact' showNav={showNav} toggleNav={toggleNav} />
+      <Page id='contact' name='contact' color='#aa6f73' upPage='about' downPage='service' showNav={showNav} toggleNav={toggleNav} />
+      <Page id='service' name='service' color='#eea990' upPage='contact' downPage='team' showNav={showNav}  toggleNav={toggleNav}  />
+      <Page id='team' name='team' color='#f6e0b5' upPage='service' downPage='team' showNav={showNav} toggleNav={toggleNav} />
     </div>
   );
 }
