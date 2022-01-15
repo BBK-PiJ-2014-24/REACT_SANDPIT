c5290f999def1b37eff7a78c860e3235aa2c7bd5
Stewart Newnham <git@newnham.com>
2022-01-15 21:23:39 +0800

    Accordion section opener, but with only one section allowed to be open at one time.

---
+++ accordion-list/src/components/Accordion.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+import {FiMinus, FiPlus} from 'react-icons/fi'
+import {Data} from '../data/data'
+import './Accordion.css'
+
+// This accordion only allows you to open one section at at a time. Version2 will allow multiple sections
+// to be open at the same time
+// ======================================================================================================
+function Accordion() {
+
+        const [clickIndex, setClickIndex] = useState(false)
+
+    const toggleAnswer = (e) => {
+        const idx = parseInt(e.target.getAttribute('id')) 
+        console.log(idx)
+
+       if(clickIndex === idx){
+           return setClickIndex(null)
+       } else {
+           setClickIndex(idx);
+       }
+    }
+    
+    return (
+        <div className='accordion'>
+            <div className='accordion-container'>
+                {Data.map((d,idx) => (
+                    <div key={idx}>
+                        <div className='question' id={idx} onClick={toggleAnswer}>
+                            <h1 id={idx}>{d.question}</h1>
+                            <span id={idx}>{idx === clickIndex ? <FiMinus id={idx} onClick={toggleAnswer} /> : <FiPlus id={idx} onClick={toggleAnswer}/>}</span>
+                        </div>
+                            {idx === clickIndex &&
+                                <div className={'answer'}>
+                                        <p>{d.answer}</p>
+                                </div>
+                            }    
+                    </div>
+                ))}            
+            </div>
+        </div>
+    )
+}
+
+export default Accordion
---
+++ accordion-list/src/data/data.js
@@ -0,0 +1,14 @@
+export const Data = [
+    {   
+        question: 'What do you a call a dog magician?',
+        answer: 'A labracadabrador'
+    },
+    {
+        question: 'What do you call a funny Mountain?',
+        answer: 'Hill-arious'   
+    },
+    {
+        question: 'What did the Astronaut say when he crashed into the moon',
+        answer: 'Apolo-gies'
+    }
+];
